d072d25bf0f52f77cd89797e61408669ef2d679d
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-11-08 00:03:27 +0900

    seedファイル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.create!(
+  [
+    {
+      title: "やること1",
+      content: "コンテント1"
+    },
+    {
+      title: "やること2",
+      content: "コンテント2"
+    },
+    {
+      title: "やること3",
+      content: "コンテント3"
+    }
+  ]
+)
