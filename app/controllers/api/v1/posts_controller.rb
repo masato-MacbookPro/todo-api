9bd8d57987cb938b5e5a19bb8e26fa2f5e72ee52
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-11-08 00:20:43 +0900

    postsのshowアクション追加

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,4 +4,15 @@ class Api::V1::PostsController < ApplicationController
     posts = Post.all.order(:id)
     render json: posts
   end
+
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
