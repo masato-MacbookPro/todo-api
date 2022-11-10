0eb382340db84076dc711488025105b6696ea225
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-11-10 14:19:08 +0900

    エラーレスポンス追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::API
   rescue_from StandardError, with: :error500
-  rescue_from ActiveRecord::RecordNotFound, with: :error404
+  rescue_from ActiveRecord::RecordNotFound, with: :error_404
+  rescue_from ActionController::RoutingError, with: :error_404
 
   private
 
-  def error404
-    render json: { errors: { code: '404', message: 'IDに一致するデータが存在しません' } }, status: :not_found
+  def error_404
+    render json: { errors: { code: '404', message: 'ページが存在しません。' } }, status: :not_found
   end
 
-  def error500(error)
-    render json: { errors: { code: '505', message: 'Internal Server Error' } }, status: :internal_server_error
+  def error_500(error)
+    render json: { errors: { code: '500', message: 'Internal Server Error' } }, status: :internal_server_error
   end
 end
