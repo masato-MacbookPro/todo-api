ee50f7f69a724a05d085dc133a55e6cf828e5e39
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-11-10 14:56:05 +0900

    rails日本語化

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,10 @@ Bundler.require(*Rails.groups)
 
 module TodoApi
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 6.1
     config.generators do |g|
       g.test_framework false
