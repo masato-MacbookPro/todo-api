7dc9a2b06b70c3687b3961447540bc6ff7e94d2b
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-11-10 14:57:09 +0900

    createアクション作成

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PostsController < ApplicationController
+  before_action :set_post, only:[:show, :update]
 
   def index
     posts = Post.all.order(:id)
@@ -6,20 +7,31 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def show
-    post = Post.find(params[:id])
-    render json: post, serializer: Api::V1::PostSerializer, status: :ok
+    render json: @post, serializer: Api::V1::PostSerializer, status: :ok
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      render json: post.errors.full_messages
+    end
   end
 
   def update
-    post = Post.find(params[:id])
-    if post.update(past_params)
-      render json: post
+    if @post.update(post_params)
+      render json: @post
     else
-      render json: post.errors
+      render json: @post.errors
     end
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
