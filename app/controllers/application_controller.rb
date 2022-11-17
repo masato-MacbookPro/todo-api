48e53523788a71e65e48f294d0ad923d1a4f8b82
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2022-11-17 22:56:27 +0900

    sign_up機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  include ActionController::RequestForgeryProtection
   rescue_from StandardError, with: :error_500
   rescue_from ActiveRecord::RecordNotFound, with: :error_404
   rescue_from ActionController::RoutingError, with: :error_404
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api, format: 'json' do
     namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: [:index]
+      end
       resources :posts
     end
   end
